96cdb3d0a4b431dd749354469be28cad3cb42dd1
Vishram7 <124357319+Vishram7@users.noreply.github.com>
2023-11-02 19:23:15 +0530

    Update WorkoutForm.js

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -54,7 +54,7 @@ const WorkoutForm = () => {
     <form className="create" onSubmit={handleSubmit}>
       <h3>Add a New Workout</h3>
 
-      <label>Excersize Title:</label>
+      <label>Exercise Title:</label>
       <input 
         type="text"
         onChange={(e) => setTitle(e.target.value)}
@@ -62,7 +62,7 @@ const WorkoutForm = () => {
         className={emptyFields.includes('title') ? 'error' : ''}
       />
 
-      <label>Load (in kg):</label>
+      <label>Weight (in kg):</label>
       <input 
         type="number"
         onChange={(e) => setLoad(e.target.value)}
